a6a5bb4f26bed8917209c27df8312ae6bd411c57
TaePoong719 <just5538@gmail.com>
2023-09-07 15:00:43 +0900

    Feat: 사진 배너와 동영상 배너 완성

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,9 +1,26 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { styled } from 'styled-components'
 
 const Nav = () => {
+  const [show, setShow] = useState(false)
+
+  useEffect(() => {
+    window.addEventListener('scroll', ()=>{
+      if(window.scrollY > 50){
+        setShow(true)
+      }else{
+        setShow(false)
+      }
+    })
+  
+    return () => {
+      // 빈 함수를 넣는게 무슨 의미지?
+      window.removeEventListener('scroll', ()=>{})
+    }
+  }, [])
+  
   return (
-    <NavWrapper>
+    <NavWrapper show={show}>
       <Logo>
         <img
           alt='Disney Plus Logo'
@@ -22,7 +39,7 @@ const NavWrapper = styled.nav`
   left: 0;
   right: 0;
   height: 70px;
-  background-color: #090b13;
+  background-color: ${props=>props.show? "#090b13" : "transparent" };
   display: flex;
   justify-content: space-between;
   align-items: center;
